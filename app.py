d59b7b0e7a42bbf4559fb9f1b568fbc067309648
franek suszko <franeksu@gmail.com>
2023-05-29 16:48:34 +0200

    added login/register functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2f03ef2cc69979cbad9d947ceaee4e84'
 
 cameras = [
     {
@@ -25,12 +27,28 @@ traffic_map = {
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('home.html', traffic_map=traffic_map, cameras=cameras)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data} successfully', category="success")
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You are now logged in', category="success")
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
