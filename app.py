0baccc7191ce057a202c2a4403d14ad4ea70c54b
franek suszko <franeksu@gmail.com>
2023-05-29 15:05:37 +0200

    added .html files, updated .gitignore and app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+cameras = [
+    {
+        'id': 1,
+        'title': 'here goes street name 1',
+        'update_date': 'here goes the last update timestamp',
+        'description': 'here goes description',
+        'image': 'here goes camera image from cv2'
+    },
+    {
+        'id': 2,
+        'title': 'here goes street name 2',
+        'update_date': 'here goes the last update timestamp',
+        'description': 'here goes description',
+        'image': 'here goes camera image from cv2'
+    }
+]
+
+traffic_map = {
+
+}
+
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('home.html', traffic_map=traffic_map, cameras=cameras)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
